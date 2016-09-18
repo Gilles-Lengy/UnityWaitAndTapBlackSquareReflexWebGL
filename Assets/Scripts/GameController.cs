ceacb085f722d9152287f718c36ef2f9d093ca35
Gilles Lengy <gilles.lengy@gmail.com>
2016-09-18 09:26:13 +0200

    diplaying the time in xx:xx:xx:xx manner in is way...

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -25,10 +25,12 @@ public class GameController : MonoBehaviour
     public float elapsedTimeTotal;
 
     /*** pour le formatage ****/
-    /* private float minutes;
-     private float seconds;
-     private float centiems;
-     */
+     private string minutes;
+     private string seconds;
+     private string centiems;
+    private string milliems;
+    private string dixmilliems;
+
 
     private bool timerOn;
 
@@ -63,11 +65,24 @@ public class GameController : MonoBehaviour
             /*
             minutes = elapsedTime / 60;
             seconds = elapsedTime % 60;
-            centiems = (elapsedTime * 100) % 1000;
+            centiems = (elapsedTime * 100) % 100;
+
+
+
+            textTimer.text = elapsedTime.ToString()+ string.Format ("{ 0:00}:{ 1:00}:{ 2:00}",minutes,seconds,centiems);
             */
+            //formatage
+            /*
+            minutes = elapsedTime / 60;
+            seconds = elapsedTime % 60;
+            centiems = (elapsedTime * 100 )%100;
+            milliems = (elapsedTime * 1000) % 1000;
 
-
+            textTimer.text = "Temps écoulé : "+string.Format ("{0:00}:{1:00}:{2:00}:{3:00}", minutes,seconds,centiems,milliems);
+            */
             textTimer.text = elapsedTime.ToString();
+
+
         }
         else {
             textTimer.text = "Wait...";
@@ -84,8 +99,48 @@ public class GameController : MonoBehaviour
                     Destroy(sprite2Hit);
                     elapsedTimeTotal += elapsedTime;
                     timerOn = false;
-                    textElapsedTimeTotal.text = elapsedTimeTotal.ToString();
+
+
+                //formatage
+                /*
+                                minutes = elapsedTimeTotal / 60;
+                                seconds = elapsedTimeTotal % 60;
+                                centiems = (elapsedTimeTotal * 100) % 100;
+
+
+
+
+                                textElapsedTimeTotal.text = string.Format("{ 0:00}:{ 1:00}:{ 2:00}", minutes, seconds, centiems);
+
+                */
+/*
+                minutes = elapsedTimeTotal / 60;
+                seconds = elapsedTimeTotal % 60;
+                centiems = (elapsedTimeTotal * 100) % 100;
+                milliems = (elapsedTimeTotal * 1000) % 1000;
+                */
+
+                string value = elapsedTimeTotal.ToString();
+                char delimiter = '.';
+                string[] substrings = value.Split(delimiter);
+                int lenght = substrings[1].Length;
+                seconds = substrings[0];
+                centiems = substrings[1].Substring(0,2);
+                milliems = substrings[1].Substring(2,2);
+               dixmilliems = substrings[1].Substring(4, lenght-4);// Lenght-4 because the lenght of the substring depends on the dixmilliems, when a 0 should be present at the end, tere is nothing... So, the lenght is variable...
+
+                if (seconds.Length < 2) {
+                    seconds = "0" + seconds;
                 }
+                if (dixmilliems.Length < 2)
+                {
+                    dixmilliems = dixmilliems + "0";
+                }
+
+                textElapsedTimeTotal.text = elapsedTimeTotal.ToString() + " / " + substrings[0] + " / " + substrings[1] + " / " + seconds + " : " + centiems + " : " + milliems + " : " + dixmilliems;
+
+
+            }
 
         }
     }
@@ -101,24 +156,20 @@ public class GameController : MonoBehaviour
             timerOn = true;
             startTime = Time.time; // on note le startTime
             sprite2HitCount++;
+            /*
             switch (sprite2HitCount) {
-                case 1:
-                    destroyWait = 1.2F;
-                    break;
-                case 3:
-                    destroyWait = 1.1F;
-                    break;
-                case 6:
+                case 7:
                     destroyWait = 0.9F;
                     break;
-                case 9:
+                case 14:
                     destroyWait = 0.88F;
                     break;
-                case 12:
+                case 21:
                     destroyWait = 0.777F;
                     break;
 
-            }
+            }*/
+            destroyWait = 0.9F;
             yield return new WaitForSeconds(destroyWait);
             if (sprite2Hit != null) { 
             Destroy(sprite2Hit);
