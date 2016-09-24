388c5094c468855b1d418da95fb5d9279386b52d
Gilles Lengy <gilles.lengy@gmail.com>
2016-09-24 10:22:07 +0200

    Function to format time
    Use for the elasped time

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -31,6 +31,7 @@ public class GameController : MonoBehaviour
     private string milliems;
     private string dixmilliems;
     private int milliemsElapsedTimeLimit;
+    private string elapsedTimeTotalFormated;
 
 
     private bool timerOn;
@@ -101,74 +102,10 @@ public class GameController : MonoBehaviour
                     Destroy(sprite2Hit);
                     elapsedTimeTotal += elapsedTime;
                     timerOn = false;
+                elapsedTimeTotalFormated =  formatTime(elapsedTimeTotal);
+                textElapsedTimeTotal.text = elapsedTimeTotalFormated;
 
-
-                //formatage
-                /*
-                                minutes = elapsedTimeTotal / 60;
-                                seconds = elapsedTimeTotal % 60;
-                                centiems = (elapsedTimeTotal * 100) % 100;
-
-
-
-
-                                textElapsedTimeTotal.text = string.Format("{ 0:00}:{ 1:00}:{ 2:00}", minutes, seconds, centiems);
-
-                */
-                /*
-                                minutes = elapsedTimeTotal / 60;
-                                seconds = elapsedTimeTotal % 60;
-                                centiems = (elapsedTimeTotal * 100) % 100;
-                                milliems = (elapsedTimeTotal * 1000) % 1000;
-                                */
-
-                string value = elapsedTimeTotal.ToString();
-                char delimiter = '.';
-                string[] substrings = value.Split(delimiter);
-                int lenghtAfterDelimiter = substrings[1].Length;
-    
-                seconds = substrings[0];
-                centiems = substrings[1].Substring(0,2);
-                milliems = substrings[1].Substring(2,2);
-
-                switch (lenghtAfterDelimiter)
-                {
-                    case 7:
-                        milliemsElapsedTimeLimit = 3;
-                        break;
-                    case 6:
-                        milliemsElapsedTimeLimit = 2;
-                        break;
-                    case 5:
-                        milliemsElapsedTimeLimit = 1;
-                        break;
-
-                }
-
-                if (lenghtAfterDelimiter == 4)
-                {
-                    dixmilliems = "00";
-                }
-                else {
-                    dixmilliems = substrings[1].Substring(4, milliemsElapsedTimeLimit);// Lenght-4 because the lenght of the substring depends on the dixmilliems, when a 0 should be present at the end, tere is nothing... So, the lenght is variable...
-                }
-                   
-
-                if (seconds.Length < 2) {
-                    seconds = "0" + seconds;
-                }
-                if (dixmilliems.Length < 2)
-                {
-                    dixmilliems = dixmilliems + "0";
-                }
-
-                /*
-                string zero = "00";
-                int lengthZero = zero.Length;
-                */
-                // textElapsedTimeTotal.text = lengthZero.ToString() + " / " +  elapsedTimeTotal.ToString() + " / " + substrings[0] + " / " + substrings[1] + " / " + lenghtAfterDelimiter.ToString() + " / " + seconds + " : " + centiems + " : " + milliems + " : " + dixmilliems;
-
-                textElapsedTimeTotal.text = seconds + " : " + centiems + " : " + milliems + " : " + dixmilliems;
+// IIIIICCCCIIII XXXXXXXXXXXXX
             }
 
         }
@@ -212,5 +149,60 @@ public class GameController : MonoBehaviour
     void setHitText()
     {
         textHitCount.text = strScore + hitCount.ToString();
+    }
+
+    private string formatTime(float timeToFormat)
+    {
+        string value = timeToFormat.ToString();
+        char delimiter = '.';
+        string[] substrings = value.Split(delimiter);
+        int lenghtAfterDelimiter = substrings[1].Length;
+
+        seconds = substrings[0];
+        centiems = substrings[1].Substring(0, 2);
+        milliems = substrings[1].Substring(2, 2);
+
+        switch (lenghtAfterDelimiter)
+        {
+            case 7:
+                milliemsElapsedTimeLimit = 3;
+                break;
+            case 6:
+                milliemsElapsedTimeLimit = 2;
+                break;
+            case 5:
+                milliemsElapsedTimeLimit = 1;
+                break;
+
+        }
+
+        if (lenghtAfterDelimiter == 4)
+        {
+            dixmilliems = "00";
+        }
+        else
+        {
+            dixmilliems = substrings[1].Substring(4, milliemsElapsedTimeLimit);// Lenght-4 because the lenght of the substring depends on the dixmilliems, when a 0 should be present at the end, tere is nothing... So, the lenght is variable...
+        }
+
+
+        if (seconds.Length < 2)
+        {
+            seconds = "0" + seconds;
+        }
+        if (dixmilliems.Length < 2)
+        {
+            dixmilliems = dixmilliems + "0";
+        }
+
+        /*
+        string zero = "00";
+        int lengthZero = zero.Length;
+        */
+        // textElapsedTimeTotal.text = lengthZero.ToString() + " / " +  elapsedTimeTotal.ToString() + " / " + substrings[0] + " / " + substrings[1] + " / " + lenghtAfterDelimiter.ToString() + " / " + seconds + " : " + centiems + " : " + milliems + " : " + dixmilliems;
+
+        string text = seconds + " : " + centiems + " : " + milliems + " : " + dixmilliems;
+
+        return text;
     }
 }
