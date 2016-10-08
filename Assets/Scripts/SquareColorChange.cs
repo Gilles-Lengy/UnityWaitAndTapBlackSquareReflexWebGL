676545506b2bc7afeafd3c8462eb04bd3f67dcc9
Gilles Lengy <gilles.lengy@gmail.com>
2016-10-08 15:35:55 +0200

    The square become white OnMouseDown

--- Assets/Scripts/SquareColorChange.cs
+++ Assets/Scripts/SquareColorChange.cs
@@ -4,19 +4,32 @@ using System.Collections;
 public class SquareColorChange : MonoBehaviour {
 
     public Color colorEnter = new Color(1.0f, 1.0f, 1.0f);// Editor !!! . Helpfull -> http://lslwiki.net/lslwiki/wakka.php?wakka=color
+    private bool mouseDowned;
 
     public Renderer rend;
     void Start()
     {
+        mouseDowned = false;
         rend = GetComponent<Renderer>();
         rend.material.color = Color.black;
     }
     void OnMouseEnter()
     {
-        rend.material.color = colorEnter;
+        if (!mouseDowned)
+        {
+            rend.material.color = colorEnter;
+        }
     }
     void OnMouseExit()
     {
-        rend.material.color = Color.black;
+        if (!mouseDowned) {
+            rend.material.color = Color.black;
+        }
+       
+    }
+    void OnMouseDown()
+    {
+        mouseDowned = true;
+        rend.material.color = Color.white;
     }
 }
